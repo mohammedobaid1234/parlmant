3d3853f4dd03a34e2acbaea67e8fef37d83c6b9e
mohammedobaid1234 <mhmd.obaid.18@gmail.com>
2022-08-22 15:24:36 +0300

    UP

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -66,6 +66,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        // 'check' => \App\Http\Middleware\CheckUserType::class
+        'check' => \App\Http\Middleware\CheckUserType::class
     ];
 }
